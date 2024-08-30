75661962075b92ed26eac144fea7c9669d3c2da7
YashasMangalore <129035779+YashasMangalore@users.noreply.github.com>
2024-08-30 13:18:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,27 +2,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const inputs = document.querySelectorAll('.code');
 
     inputs.forEach((input, index) => {
-        // Handle input changes
         input.addEventListener('input', () => {
             if (input.value.length === 1 && index < inputs.length - 1) {
                 inputs[index + 1].focus();
             }
         });
 
-        // Handle backspace key press
         input.addEventListener('keydown', (e) => {
             if (e.key === 'Backspace' && input.value === '' && index > 0) {
                 inputs[index - 1].focus();
             }
         });
 
-        // Add focused class on focus
         input.addEventListener('focus', () => {
             inputs.forEach(input => input.classList.remove('focused')); // Remove 'focused' class from all inputs
             input.classList.add('focused'); // Add 'focused' class to the currently focused input
         });
 
-        // Remove focused class on blur
         input.addEventListener('blur', () => {
             input.classList.remove('focused');
         });
